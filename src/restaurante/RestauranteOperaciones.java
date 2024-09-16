d6ff571fd2302bebace9b8d45fa5d108c48b3152
yuliavolos8 <147557438+yuliavolos8@users.noreply.github.com>
2024-09-16 22:39:40 +0200

    metodos de interfaz

--- src/restaurante/RestauranteOperaciones.java
+++ src/restaurante/RestauranteOperaciones.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import Menu.Plato;
 import persona.Cliente;
 import persona.Empleado;
-
+// metodos que tienen que ser implementados en clase Restaurante
 public interface RestauranteOperaciones {
     public void hacerReserva(Cliente cliente ,FechaReserva fecha);
     public String tomarPedido(Empleado empleado, Cliente cliente, Map<Plato, Integer> mapPlatos);
